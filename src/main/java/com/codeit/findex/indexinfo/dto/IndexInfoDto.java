ebad517ff9c5b8e292e24f1d99450c39f16227ef
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-09-08 21:41:39 +0900

    feat: IndexInfo 관련 DTO 추가 - Update DTO - Create DTO - Response DTO - Summary DTO

--- src/main/java/com/codeit/findex/indexinfo/dto/IndexInfoDto.java
+++ src/main/java/com/codeit/findex/indexinfo/dto/IndexInfoDto.java
@@ -1,4 +1,18 @@
 package com.codeit.findex.indexinfo.dto;
 
-public class IndexInfoDto {
-}
+import com.codeit.findex.common.enums.SourceType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record IndexInfoDto(
+        Long id,
+        String indexClassification,
+        String indexName,
+        Integer employedItemsCount,
+        LocalDate basePointInTime,
+        BigDecimal baseIndex,
+        SourceType sourceType,
+        Boolean favorite,
+        Boolean autoSyncEnabled
+) {}
