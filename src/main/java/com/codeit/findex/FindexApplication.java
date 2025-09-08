aeb5e44d6bbeb14c080f52d00d46763510a629d1
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-09-08 11:16:41 +0900

    chore: API 키 환경변수 설정 추가 - 환경변수 설정 & 테스트 코드 추가

--- src/main/java/com/codeit/findex/FindexApplication.java
+++ src/main/java/com/codeit/findex/FindexApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FindexApplication {
 
     public static void main(String[] args) {
+        // API-KEY 출력 테스트
+        System.out.println("API KEY = " + System.getenv("FINDEX_API_KEY"));
         SpringApplication.run(FindexApplication.class, args);
     }
 
