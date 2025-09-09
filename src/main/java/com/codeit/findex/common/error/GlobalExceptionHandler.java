4c615c91e0576b0ff7257314a232fd7305f4c301
KimDongGyu <redmatoda@naver.com>
2025-09-09 10:30:12 +0900

    feat: ErrorResponse, GlobalExceptionHandler 추가

--- src/main/java/com/codeit/findex/common/error/GlobalExceptionHandler.java
+++ src/main/java/com/codeit/findex/common/error/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.codeit.findex.common.error;
 
-import com.codeit.findex.common.error.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
