b9b99fe57ad449e7f925bf481d35e38bbd182947
rlawnsry <color9117@naver.com>
2025-09-09 18:09:31 +0900

    feat: api 전체 데이터 파싱 및 출력

--- src/main/java/com/codeit/findex/data/ApiFetchService.java
+++ src/main/java/com/codeit/findex/data/ApiFetchService.java
@@ -1,11 +1,13 @@
 package com.codeit.findex.data;
 
 import jakarta.annotation.PostConstruct;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
@@ -21,6 +23,9 @@ public class ApiFetchService {
     @Value("${findex.api.url}")
     private String apiUrl;
 
+    private int count = 1;
+    private final int MAX_RETRY = 3;
+
     @PostConstruct
     public void init() throws Exception {
         System.out.println("fetchAndProcessAll 시작");
@@ -31,20 +36,20 @@ public class ApiFetchService {
 
     public void fetchAndProcessAll() throws Exception {
         int pageNo = 1;
-        int pageSize = 100;
+        int pageSize = 1000;
         int totalPages = Integer.MAX_VALUE;
+        int numOfRows = 1000;
 
         while (pageNo <= totalPages) {
-            StringBuilder urlBuilder = buildUrl(pageNo, pageSize);
-            String responseJson = callApi(urlBuilder);
+            StringBuilder urlBuilder = buildUrl(pageNo, numOfRows);
+            String responseJson = callApiWithRetry(urlBuilder);
             System.out.println("API 호출 시도: page=" + pageNo);
             if(responseJson == null || responseJson.isEmpty()) {
                 System.out.println("responseJson에 문제 발생.");
                 break;
             }
-            System.out.println("API 응답 없음 또는 호출 실패");
+            System.out.println("API 호출 성공, 응답 길이: " + responseJson.length());
 
-            System.out.println("파싱 시작 - 전체 응답 문자열: " + responseJson);
             Body body = findexApiParser.parseBody(responseJson);
             System.out.println("총 데이터 수: " + body.getTotalCount());
 
@@ -62,11 +67,36 @@ public class ApiFetchService {
         }
     }
 
-    private StringBuilder buildUrl(int pageNo, int pageSize) throws UnsupportedEncodingException {
+    private String callApiWithRetry(StringBuilder urlBuilder) throws InterruptedException {
+        int retryCount = 0;
+
+        while (retryCount < MAX_RETRY) {
+            String responseJson = callApi(urlBuilder);
+
+            if(responseJson != null && !responseJson.isEmpty()) {
+                if(isValidResponse(responseJson)) {
+                    return responseJson;
+                } else {
+                    System.err.println("API 내부 오류 감지. 응답 내용 로그 출력:\n" + responseJson);
+                }
+            } else {
+                System.err.println("빈 응답 감지. 재시도 " + (retryCount+ 1));
+            }
+
+            retryCount++;
+            Thread.sleep(1000L * retryCount);
+
+        }
+
+        System.err.println("최대 재시도 도달. API 호출 실패.");
+        return null;
+    }
+
+    private StringBuilder buildUrl(int pageNo, int numOfRows) throws UnsupportedEncodingException {
         StringBuilder urlBuilder = new StringBuilder(apiUrl);
         urlBuilder.append("?serviceKey=").append(apiKey);
         urlBuilder.append("&pageNo=").append(pageNo);
-        urlBuilder.append("&pageSize=").append(pageSize);
+        urlBuilder.append("&numOfRows=").append(numOfRows);
         urlBuilder.append("&resultType=json");
 
         return urlBuilder;
@@ -80,9 +110,8 @@ public class ApiFetchService {
             conn.setRequestProperty("Content-type", "application/json");
             int code = conn.getResponseCode();
             System.out.println("Response code: " + code);
-            if(code != 200) {
-                System.out.println("요청에 실패하였습니다.");
-                return null;
+            if(code != HttpURLConnection.HTTP_OK) {
+                throw new IOException("HTTP 오류 코드: " + code);
             }
             BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
             StringBuilder sb = new StringBuilder();
@@ -99,10 +128,23 @@ public class ApiFetchService {
         }
     }
 
+    private boolean isValidResponse(String responseJson) {
+        JSONObject json = new JSONObject(responseJson);
+        JSONObject header = json.getJSONObject("response").getJSONObject("header");
+        String resultCode = header.optString("resultCode", "");
+        String resultMsg = header.optString("resultMsg", "");
+
+        if(!resultCode.equals("00")) {
+            System.err.println("API 장애 또는 오류 반환:  코드 = " + resultCode + ", 메시지 = " +  resultMsg);
+            return false;
+        }
+        return true;
+    }
+
     private void processItems(int pageNo, List<Item> items) {
         for (Item item : items) {
             System.out.println("---- Item ----");
-            System.out.println("pageNo: " + pageNo + ", Index Name: " + item.getIdxNm());
+            System.out.println("pageNo: " + pageNo + ", count : "+ count +  ", Index Name: " + item.getIdxNm());
             System.out.println("Base Date: " + item.getBasDt());
             System.out.println("Closing Price: " + item.getClpr());
             System.out.println("Opening Price: " + item.getMkp());
@@ -111,6 +153,7 @@ public class ApiFetchService {
             System.out.println("Volume: " + item.getTrqu());
             System.out.println("Trading Amount: " + item.getTrPrc());
             System.out.println("--------------------");
+            count++;
         }
     }
 }
--- src/main/java/com/codeit/findex/data/FindexApiParser.java
+++ src/main/java/com/codeit/findex/data/FindexApiParser.java
@@ -28,6 +28,7 @@ public class FindexApiParser {
     public List<Item> parseItems(String jsonString) {
         JSONObject jsonObject = new JSONObject(jsonString);
         JSONObject response = jsonObject.getJSONObject("response");
+
         JSONObject body = response.getJSONObject("body");
 
         int numOfRows = body.getInt("numOfRows");
@@ -39,41 +40,54 @@ public class FindexApiParser {
         System.out.println("totalCount: " + totalCount);
 
         JSONObject items = body.getJSONObject("items");
-        JSONArray itemArray = items.getJSONArray("item");
+        Object itemObj = items.get("item");
 
         List<Item> itemList = new ArrayList<>();
-        for(int i = 0; i < itemArray.length(); i++) {
-            JSONObject obj = itemArray.getJSONObject(i);
-            Double lsYrEdVsFltRt = getDoubleData(obj, "lsYrEdVsFltRt");
-            String basPntm = getStrData(obj, "basPntm");
-            Double basIdx = getDoubleData(obj, "basIdx");
-            LocalDate basDt = getLocalDateData(obj, "basDt");
-            String idxCsf = getStrData(obj, "idxCsf");
-            String idxNm = getStrData(obj, "idxNm");
-            Integer epyItmsCnt = getIntData(obj, "epyItmsCnt");
-            Double clpr = getDoubleData(obj, "clpr");
-            Double vs = getDoubleData(obj, "vs");
-            Double fltRt = getDoubleData(obj, "fltRt");
-            Double mkp = getDoubleData(obj, "mkp");
-            Double hipr = getDoubleData(obj, "hipr");
-            Double lopr = getDoubleData(obj, "lopr");
-            Long trqu = getLongData(obj, "trqu");
-            Long trPrc = getLongData(obj, "trPrc");
-            Long lstgMrktTotAmt = getLongData(obj, "lstgMrktTotAmt");
-            Integer lsYrEdVsFltRg = getIntData(obj, "lsYrEdVsFltRg");
-            Double yrWRcrdHgst = getDoubleData(obj, "yrWRcrdHgst");
-            String yrWRcrdHgstDt = getStrData(obj, "yrWRcrdHgstDt");
-            Integer yrWRcrdLwst = getIntData(obj, "yrWRcrdLwst");
-            String yrWRcrdLwstDt = getStrData(obj, "yrWRcrdLwstDt");
-
-            Item item = new Item(
-                    lsYrEdVsFltRt, basPntm, basIdx, basDt, idxCsf, idxNm, epyItmsCnt, clpr, vs, fltRt,
-                    mkp, hipr, lopr, trqu, trPrc, lstgMrktTotAmt, lsYrEdVsFltRg, yrWRcrdHgst, yrWRcrdHgstDt,
-                    yrWRcrdLwst, yrWRcrdLwstDt
-            );
-            itemList.add(item);
+        if (itemObj instanceof JSONArray) {
+            JSONArray arr = (JSONArray) itemObj;
+            for (int i = 0; i < arr.length(); i++) {
+                JSONObject obj = arr.getJSONObject(i);
+                Item item = parseItem(obj);
+                itemList.add(item);
+            }
+        } else if (itemObj instanceof JSONObject) {
+            itemList.add(parseItem((JSONObject) itemObj));
+        } else {
+            System.out.println("알 수 없는 item 타입: " + itemObj.getClass());
         }
+
         return itemList;
+    }
+
+    private Item parseItem(JSONObject obj) {
+        Double lsYrEdVsFltRt = getDoubleData(obj, "lsYrEdVsFltRt");
+        String basPntm = getStrData(obj, "basPntm");
+        Double basIdx = getDoubleData(obj, "basIdx");
+        LocalDate basDt = getLocalDateData(obj, "basDt");
+        String idxCsf = getStrData(obj, "idxCsf");
+        String idxNm = getStrData(obj, "idxNm");
+        Integer epyItmsCnt = getIntData(obj, "epyItmsCnt");
+        Double clpr = getDoubleData(obj, "clpr");
+        Double vs = getDoubleData(obj, "vs");
+        Double fltRt = getDoubleData(obj, "fltRt");
+        Double mkp = getDoubleData(obj, "mkp");
+        Double hipr = getDoubleData(obj, "hipr");
+        Double lopr = getDoubleData(obj, "lopr");
+        Long trqu = getLongData(obj, "trqu");
+        Long trPrc = getLongData(obj, "trPrc");
+        Long lstgMrktTotAmt = getLongData(obj, "lstgMrktTotAmt");
+        Integer lsYrEdVsFltRg = getIntData(obj, "lsYrEdVsFltRg");
+        Double yrWRcrdHgst = getDoubleData(obj, "yrWRcrdHgst");
+        String yrWRcrdHgstDt = getStrData(obj, "yrWRcrdHgstDt");
+        Integer yrWRcrdLwst = getIntData(obj, "yrWRcrdLwst");
+        String yrWRcrdLwstDt = getStrData(obj, "yrWRcrdLwstDt");
+
+        Item item = new Item(
+                lsYrEdVsFltRt, basPntm, basIdx, basDt, idxCsf, idxNm, epyItmsCnt, clpr, vs, fltRt,
+                mkp, hipr, lopr, trqu, trPrc, lstgMrktTotAmt, lsYrEdVsFltRg, yrWRcrdHgst, yrWRcrdHgstDt,
+                yrWRcrdLwst, yrWRcrdLwstDt
+        );
+        return item;
     }
 
     private String getStrData(JSONObject obj, String key) {
