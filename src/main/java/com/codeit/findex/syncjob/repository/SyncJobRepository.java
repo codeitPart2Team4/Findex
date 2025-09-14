91435f3381142c97d74c25dacbf456dbccac6474
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-09-15 01:10:08 +0900

    feat: IndexInfo 삭제 시 연관 데이터 벌크 삭제 메서드 추가

--- src/main/java/com/codeit/findex/syncjob/repository/SyncJobRepository.java
+++ src/main/java/com/codeit/findex/syncjob/repository/SyncJobRepository.java
@@ -3,6 +3,9 @@ package com.codeit.findex.syncjob.repository;
 import com.codeit.findex.indexinfo.entity.IndexInfo;
 import com.codeit.findex.syncjob.entity.SyncJob;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.Optional;
@@ -12,4 +15,8 @@ public interface SyncJobRepository extends JpaRepository<SyncJob, Long>, SyncJob
     Optional<SyncJob> findByIndexInfoAndJobType(IndexInfo indexInfo, String jobType);
 
     Optional<SyncJob> findByIndexInfoAndJobTypeAndTargetDate(IndexInfo indexInfo, String jobType, LocalDate targetDate);
+
+    @Modifying
+    @Query("delete from SyncJob sj where sj.indexInfo.id = :indexInfoId")
+    void deleteByIndexInfoId(@Param("indexInfoId") Long indexInfoId);
 }
